900657e51c41b3cf2bfca9a3aa4e787f62fe96d4
student <student@CAB4-COMP10>
2025-10-07 16:09:17 +1000

    Добавьте файлы проекта.

---
+++ MauiApp1/Pages/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using MauiApp1.Models;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+      public List<Movies> MoviesList = new List<Movies>();
+      private  int IdMovie = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void SaveMovie()
+        {
+            Movies movies = new Movies();
+            movies.Id = IdMovie;
+            movies.Name = TitleText.Text;
+            movies.Description = DiscriptionText.Text;
+            movies.Date = DiscriptionDate.Date;
+
+            MoviesList.Add(movies);
+            SaveFileMovie();
+            IdMovie++;
+        }
+  
+
+        private void Button_Clicked_Movie(object sender, EventArgs e)
+        {
+            SaveMovie();
+           
+        }
+
+     
+        private async void SaveFileMovie()
+        {
+            string text = "";
+
+            string targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, "movie.db");
+            using FileStream outputStream = File.OpenWrite(targetFile);
+            using StreamWriter streamWriter = new StreamWriter(outputStream);
+            for (int i = 0; i > MoviesList.Count; i++)
+            {
+                text = $"{MoviesList[i].Id}";
+                await streamWriter.WriteAsync(text);
+                text = $"{MoviesList[i].Name}";
+                await streamWriter.WriteAsync(text);
+                text = $"{MoviesList[i].Description}";
+                await streamWriter.WriteAsync(text);
+                text = $"{MoviesList[i].Date}";
+                await streamWriter.WriteAsync(text);
+             
+            }
+           
+        }
+       
+        public async void Button_Clicked_To_Page2(object sender, EventArgs e)
+        {
+            //new NavigationPage(new NewPage1());
+            await Navigation.PushModalAsync(new NewPage1());
+        }
+    }
+
+    }
+
---
+++ MauiApp1/Pages/NewPage1.xaml.cs
@@ -0,0 +1,51 @@
+using MauiApp1.Models;
+namespace MauiApp1;
+
+public partial class NewPage1 : ContentPage
+{
+    public List<Author> AuthorList = new List<Author>();
+    private int IdAuthor = 0;
+    public NewPage1()
+	{
+		InitializeComponent();
+	}
+    public void SaveAuthor()
+    {
+        Author author = new Author();
+        author.Id = IdAuthor;
+        author.Name = Name.Text;
+        author.SecondName = SecondName.Text;
+        author.ThrityName = ThirtyName.Text;
+        author.BirthDay = BirthdayDate.Date;
+
+
+        AuthorList.Add(author);
+        SaveFileAuthor();
+        IdAuthor++;
+    }
+    private void Button_Clicked_Author(object sender, EventArgs e)
+    {
+        SaveAuthor();
+    }
+    private async void SaveFileAuthor()
+    {
+        string text = "";
+
+        string targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, "author.db");
+        using FileStream outputStream = File.OpenWrite(targetFile);
+        using StreamWriter streamWriter = new StreamWriter(outputStream);
+        for (int i = 0; i > AuthorList.Count; i++)
+        {
+            text = $"{AuthorList[i].Id}";
+            await streamWriter.WriteAsync(text);
+            text = $"{AuthorList[i].Name}";
+            await streamWriter.WriteAsync(text);
+            text = $"{AuthorList[i].SecondName}";
+            await streamWriter.WriteAsync(text);
+            text = $"{AuthorList[i].BirthDay}";
+            await streamWriter.WriteAsync(text);
+
+        }
+
+    }
+}
